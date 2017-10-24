4121c408925cc269e5ba528262c8f5c20de2e386
bohdan <belokur.bogdan@yandex.ru>
2017-10-25 01:24:03 +0300

    parser without avoiding block

--- category_parser.py
+++ category_parser.py
@@ -3,7 +3,7 @@ from lxml import etree
 from phone_parser import get_phone
 
 headers = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Mobile Safari/537.36',
                 'Accept': 'application/json',
                 'Accept-Language': "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
                 'Accept-Encoding': 'gzip, deflate, br',
@@ -18,11 +18,14 @@ headers = {
 
 def parse_category(link):
     response = requests.get(link, headers=headers)
-# root = etree.Element("root")
+    print(response.text)
     root = etree.HTML(response.text)
     for i in range(1,22):
         url = root.xpath("/html/body/section/article[{}]/div/a/@href".format(str(i)))[0]
         phone = get_phone(url, headers)
-        print(url, phone)
+        with open('phone.txt') as f:
+            f.write(phone)
+        print(phone)
 
-parse_category('https://m.avito.ru/moskva/transport')
+for i in range(1, 10):
+    parse_category('https://m.avito.ru/moskva/transport?p={}'.format(str(i)))
--- phone_parser.py
+++ phone_parser.py
@@ -7,7 +7,7 @@ def get_phone(relation_link, fake_haders):
     item_page = requests.get(avito + relation_link)
     print(item_page.url)
     root = etree.HTML(item_page.text)
-    url = root.xpath("/html/body/section/article/section[4]/div[2]/div/a[1]/@href")[0] + '?async'
+    url = root.xpath("//a[contains(@class, 'js-action-show-number')]/@href")[0] + '?async'
     data = {'async': ''}
     response = requests.get('https://m.avito.ru{}'.format(url), headers=fake_haders, data=data)
     return response.text
